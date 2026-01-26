f829ed58785e2c96ed23915644c44145d404760f
Sebastian Ortiz Valencia <sebastianortiz989@gmail.com>
2026-01-26 12:43:12 +0000

    chore: Update build artifacts and add i18n translations
    Remove old build chunks from service portal and update DocType JS files. Add auto-generated translation files for all supported languages. Remove deprecated case-management component files.
    
    - Add 83 translation CSV files for internationalization
    - Update Service Portal and Service Portal Tool JS files
    - Remove old Angular build chunks
    - Remove unused case-management component files

--- common_configurations/common_configurations/doctype/service_portal/service_portal.js
+++ common_configurations/common_configurations/doctype/service_portal/service_portal.js
@@ -1,8 +1,65 @@
 // Copyright (c) 2026, Sebastian Ortiz Valencia and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Service Portal", {
-// 	refresh(frm) {
-//
-// 	},
-// });
+// Child table handler for Service Portal Tool
+frappe.ui.form.on('Service Portal Tool', {
+	tool_type: function(frm, cdt, cdn) {
+		console.log('=== Service Portal Tool - tool_type changed ===');
+		console.log('cdt:', cdt);
+		console.log('cdn:', cdn);
+
+		// When tool_type changes, fetch and populate fields if they're empty
+		const row = locals[cdt][cdn];
+		console.log('Row data:', row);
+		console.log('Selected tool_type:', row.tool_type);
+
+		if (row.tool_type) {
+			console.log('Fetching Tool Type data for:', row.tool_type);
+
+			frappe.db.get_value('Tool Type', row.tool_type, ['tool_label', 'description', 'icon'], (r) => {
+				console.log('Tool Type response:', r);
+
+				if (r) {
+					console.log('Current row values before update:', {
+						label: row.label,
+						tool_description: row.tool_description,
+						icon: row.icon
+					});
+
+					// Only set label if it's empty
+					if (!row.label) {
+						console.log('Setting label to:', r.tool_label);
+						frappe.model.set_value(cdt, cdn, 'label', r.tool_label);
+					} else {
+						console.log('Label already has value, skipping');
+					}
+
+					// Only set description if it's empty
+					if (!row.tool_description) {
+						console.log('Setting tool_description to:', r.description);
+						frappe.model.set_value(cdt, cdn, 'tool_description', r.description);
+					} else {
+						console.log('Description already has value, skipping');
+					}
+
+					// Only set icon if it's empty
+					if (!row.icon) {
+						console.log('Setting icon to:', r.icon);
+						frappe.model.set_value(cdt, cdn, 'icon', r.icon);
+					} else {
+						console.log('Icon already has value, skipping');
+					}
+
+					// Refresh the grid row to show the updated values
+					console.log('Refreshing tools field');
+					frm.refresh_field('tools');
+					console.log('=== Update complete ===');
+				} else {
+					console.log('ERROR: No data returned from Tool Type');
+				}
+			});
+		} else {
+			console.log('No tool_type selected');
+		}
+	}
+});
--- common_configurations/common_configurations/doctype/service_portal_tool/service_portal_tool.js
+++ common_configurations/common_configurations/doctype/service_portal_tool/service_portal_tool.js
@@ -1,25 +1,61 @@
 frappe.ui.form.on('Service Portal Tool', {
-	tool_type: function(frm) {
+	tool_type: function(frm, cdt, cdn) {
+		console.log('=== Service Portal Tool - tool_type changed ===');
+		console.log('cdt:', cdt);
+		console.log('cdn:', cdn);
+
 		// When tool_type changes, fetch and populate fields if they're empty
-		if (frm.doc.tool_type) {
-			frappe.db.get_value('Tool Type', frm.doc.tool_type, ['tool_label', 'description', 'icon'], (r) => {
+		const row = locals[cdt][cdn];
+		console.log('Row data:', row);
+		console.log('Selected tool_type:', row.tool_type);
+
+		if (row.tool_type) {
+			console.log('Fetching Tool Type data for:', row.tool_type);
+
+			frappe.db.get_value('Tool Type', row.tool_type, ['tool_label', 'description', 'icon'], (r) => {
+				console.log('Tool Type response:', r);
+
 				if (r) {
+					console.log('Current row values before update:', {
+						label: row.label,
+						tool_description: row.tool_description,
+						icon: row.icon
+					});
+
 					// Only set label if it's empty
-					if (!frm.doc.label) {
-						frm.set_value('label', r.tool_label);
+					if (!row.label) {
+						console.log('Setting label to:', r.tool_label);
+						frappe.model.set_value(cdt, cdn, 'label', r.tool_label);
+					} else {
+						console.log('Label already has value, skipping');
 					}
 
 					// Only set description if it's empty
-					if (!frm.doc.tool_description) {
-						frm.set_value('tool_description', r.description);
+					if (!row.tool_description) {
+						console.log('Setting tool_description to:', r.description);
+						frappe.model.set_value(cdt, cdn, 'tool_description', r.description);
+					} else {
+						console.log('Description already has value, skipping');
 					}
 
 					// Only set icon if it's empty
-					if (!frm.doc.icon) {
-						frm.set_value('icon', r.icon);
+					if (!row.icon) {
+						console.log('Setting icon to:', r.icon);
+						frappe.model.set_value(cdt, cdn, 'icon', r.icon);
+					} else {
+						console.log('Icon already has value, skipping');
 					}
+
+					// Refresh the grid row to show the updated values
+					console.log('Refreshing tools field');
+					frm.refresh_field('tools');
+					console.log('=== Update complete ===');
+				} else {
+					console.log('ERROR: No data returned from Tool Type');
 				}
 			});
+		} else {
+			console.log('No tool_type selected');
 		}
 	}
 });
